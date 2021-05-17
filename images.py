8c9e8906026660a2abc42ddb246db47654802a71
Kevin Jacob <38008146+kevinthetesla@users.noreply.github.com>
2021-05-17 10:26:30 +0530

    Update images.py

--- images.py
+++ images.py
@@ -2,10 +2,10 @@ import requests
 import json
 url = "https://bing-image-search1.p.rapidapi.com/images/search"
 
-querystring = {"q":"mangoes","count": 200}
+querystring = {"q":"YOUR QUERY HERE","count": }
 
 headers = {
-    'x-rapidapi-key': "8d718b9663msh96c9ff6652097dbp1d7662jsn93807b8c1a1c",
+    'x-rapidapi-key': "YOUR BING IMAGE SEARCH API KEY HERE",
     'x-rapidapi-host': "bing-image-search1.p.rapidapi.com"
     }
 
